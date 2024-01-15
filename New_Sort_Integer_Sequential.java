c30c8fb3265b9c67adb673eda265b70d0be21adf
RafaelNovais <54107877+RafaelNovais@users.noreply.github.com>
2024-01-15 20:31:41 +0000

    Update New_Sort_Integer_Sequential.java

--- New_Sort_Integer_Sequential.java
+++ New_Sort_Integer_Sequential.java
@@ -1,26 +1,67 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 public class New_Sort_Integer_Sequential {
+	/*Q3*/
 
 	public static void main(String[] args) {
 
-		// We test the sorting procedure with a list of random integer objects
 		Integer n = 100000;
 
 
-		Comparator<Integer> comparator = (x, y) -> Integer.compare(x, y);
+		Comparator<Integer> comparator = Integer::compare;
+
 
 		ArrayList<Integer> list = new ArrayList<>();
 		for (int i = 0; i < n; i++)
 			list.add((int) Math.ceil(Math.random() * n));
 
-		sort(list, 0, n - 1, comparator);
+
+		parallelSort(list, comparator);
+
 
 		for (int i = 0; i < n; i++) {
 			System.out.print(list.get(i) + ", ");
 			if (i > 0 && i % 20 == 0)
 				System.out.println();
+		}
+	}
+
+	static void parallelSort(ArrayList<Integer> list, Comparator<Integer> comparator) {
+		ForkJoinPool pool = new ForkJoinPool();
+		pool.invoke(new ParallelSortTask(list, 0, list.size() - 1, comparator));
+		pool.shutdown();
+	}
+
+	static class ParallelSortTask extends RecursiveAction {
+		private static final int THRESHOLD = 1000;
+		private final ArrayList<Integer> list;
+		private final int left;
+		private final int right;
+		private final Comparator<Integer> comparator;
+
+		public ParallelSortTask(ArrayList<Integer> list, int left, int right, Comparator<Integer> comparator) {
+			this.list = list;
+			this.left = left;
+			this.right = right;
+			this.comparator = comparator;
+		}
+
+		@Override
+		protected void compute() {
+			if (right - left <= THRESHOLD) {
+
+				New_Sort_Integer_Sequential.sort(list, left, right, comparator);
+			} else {
+
+				int s = New_Sort_Integer_Sequential.part(list, left, right, comparator);
+				ParallelSortTask leftTask = new ParallelSortTask(list, left, s - 1, comparator);
+				ParallelSortTask rightTask = new ParallelSortTask(list, s + 1, right, comparator);
+
+				invokeAll(leftTask, rightTask);
+			}
 		}
 	}
 
@@ -41,8 +82,7 @@ public class New_Sort_Integer_Sequential {
 		Integer i = left - 1, j = right;
 
 		for (;;) {
-			while (comparator.compare(list.get(++i), list.get(right)) < 0)
-				;
+			while (comparator.compare(list.get(++i), list.get(right)) < 0);
 
 			while (comparator.compare(list.get(right), list.get(--j)) < 0)
 				if (j.equals(left))
